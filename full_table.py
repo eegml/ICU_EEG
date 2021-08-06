d49cec4d91e740b5b076eb3fff4a8cefc2a46d3b
Chris Lee-Messer <chris@lee-messer.net>
2021-08-06 15:46:56 -0700

    update combined patient-annoations tables to use John's cleaned up versions

--- full_table.py
+++ full_table.py
@@ -15,10 +15,10 @@
 import pandas as pd
 import numpy as np
 import yaml
-import
+import json
 
 # %% [markdown]
-# There are some subjects in the repository which were not ultimately used in the published form of this project (in particular, anything with 'IIC' in it - these were interictal - ictal continuum patients which we did not want to use in training the model), as well as a few others which clinicians on our team judged to have insufficient data quality. We also had to re-upload files recently under new IDs on ieeg.org for deidentification purposes which may have made some of the annotation files fail. 
+# Early notes: There are some subjects in the repository which were not ultimately used in the published form of this project (in particular, anything with 'IIC' in it - these were interictal - ictal continuum patients which we did not want to use in training the model), as well as a few others which clinicians on our team judged to have insufficient data quality. We also had to re-upload files recently under new IDs on ieeg.org for deidentification purposes which may have made some of the annotation files fail. 
 #
 # It looks like the annotations are labeled in seconds.
 # I'm not sure what the ii_start and ii_start are for.
@@ -42,6 +42,7 @@ ptdatadict = ptdf2.to_dict(orient='index')
 
 # %%
 len(ptdatadict)
+ptdatadict
 
 # %%
 for anum,data in annotations.items():
@@ -52,6 +53,11 @@ for anum,data in annotations.items():
         data['portal_id'] = ptdatadict[num]['portal_id']
         data['sz_presence'] = ptdatadict[num]['sz_presence']
         data['diagnosis'] = ptdatadict[num]['diagnosis']
+        if ptdatadict[num]['sz_presence']:
+            data['pt_type'] = 1
+        else:
+            data['pt_type'] = 2
+        del data['type']
 
 # %%
 yaml.safe_dump(annotations, open('patient_annotations.yaml','w+'))
